9ef43a5dd1273ef2f4ecfbfd0adac4278d993c08
ManuelaZapatAtencia <manuela.zapata.atencia@gmail.com>
2023-09-19 17:31:32 -0500

    Clase "ContextoBaseDatos"

---
+++ Shared/Models/IDatos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_Enun8.Shared.Models
+{
+    public class IDatos
+    {
+        void ActualizarCliente(Cliente cliente);
+
+        void agregarCliente(Cliente cliente);
+
+        void EliminarCliente(int Id_Cliente);
+
+        Cliente GetCliente(int Id_Cliente);
+
+
+
+    }
+}
